26cc52d6cca8b01855c1f21379c473585970aa65
Nicolas Cordeiro <32917658+Nicolasck-dev@users.noreply.github.com>
2024-11-12 21:54:03 -0300

    Add files via upload

---
+++ SistemaDeCadastroSenac2024/src/controller/TelaDeMenuController.java
@@ -0,0 +1,12 @@
+package controller;
+import view.*;
+import model.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TelaDeMenuController extends TelaDeMenuView {
+    
+    
+}
